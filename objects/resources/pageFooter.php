d8f7fd36ec3095d46dbcc82380f598904de19f42
LordBohmies <victor.bohmies@gmail.com>
2017-05-10 17:23:16 +0200

    First commit
    simple footer and header

---
+++ objects/resources/pageFooter.php
@@ -0,0 +1,28 @@
+<!--
+******************************************************
+                        FOOTER
+******************************************************
+-->
+
+    <div id="footer">
+        <div class="container">
+            <div class="rev">
+                <div class="col-lg-12 col-md-12">
+                    provided by:
+                    <a href="#" target="_blank" style="color:rgba(255,255,255,1.0)">Victor Bohmies</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!--
+******************************************************
+                      JAVASCRIPT
+******************************************************
+-->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+
+</body>
+</head>
---
+++ objects/resources/pageHeader.php
@@ -0,0 +1,20 @@
+<!--
+******************************************************
+                        HEADER
+******************************************************
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <met http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+
+        <title>Gnagsåret bokning</title>
+
+        <!-- STYLE -->
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <!-- Custom -->
+        <link href="assets/css/style.css" rel="stylesheet" />
+    </head>
